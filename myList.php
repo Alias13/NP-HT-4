dbe5f506368d121a60298d7d7cd998f47ff86c69
Alias <vlavrov.ino@gmail.com>
2016-10-14 19:49:14 +0200

    update

--- myList.php
+++ myList.php
@@ -1,8 +1,16 @@
 <?php
 class myList
 {
-    public $list = array();
+    private $list = array();
 
+    public function isEmpty(){
+        if(!empty($this->list)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public function addToEnd($listItem)
     {
         $arr = array();
@@ -18,12 +26,35 @@ class myList
             $this->list[count($this->list) - 2][1] = &$arr[0];
         }
     }
+    public function addToStart($listItem){
+        {
+            $arr = array();
+            if (!$this->isEmpty()){
+                $arr[] = $listItem;
+                $arr[] = 0;
+                $this->list[] = $arr;
+            }
+            else {
+                $arr[] = $listItem;
+                $arr[] = $this->list[0][0];
+                array_unshift($this->list, $arr);
+            }
+        }
+    }
+    public function deleteItem($listItem){
+        if (!in_array($listItem, $this->list)){
+            return false;
+        }
+        else{
+         #   array_search();
+        }
+    }
 }
 
-$newList = new myList();
-$newList->addToEnd("aaa");
-$newList->addToEnd("bbb");
-$newList->addToEnd("ccc");
-$newList->addToEnd("ddd");
-
-var_dump($newList->list);
+#$newList = new myList();
+#$newList->addToEnd("aaa");
+#$newList->addToStart("bbb");
+#$newList->addToStart("ccc");
+#$newList->addToEnd("ddd");
+#var_dump($newList->isEmpty());
+#var_dump($newList->list);
